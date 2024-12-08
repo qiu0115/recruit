21c30f0ad52ef3f6962733226b64dde84a13977d
深藏buled蓝先生 <935988804@qq.com>
2024-12-08 13:51:08 +0800

    项目初始化

---
+++ src/main/java/com/queryx/recruit/RecruitApplication.java
@@ -0,0 +1,23 @@
+package com.queryx.recruit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecruitApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(RecruitApplication.class, args);
+        System.out.println("(♥◠‿◠)ﾉﾞ  系统模块启动成功   ლ(´ڡ`ლ)ﾞ  \n" +
+                " .-------.       ____     __        \n" +
+                " |  _ _   \\      \\   \\   /  /    \n" +
+                " | ( ' )  |       \\  _. /  '       \n" +
+                " |(_ o _) /        _( )_ .'         \n" +
+                " | (_,_).' __  ___(_ o _)'          \n" +
+                " |  |\\ \\  |  ||   |(_,_)'         \n" +
+                " |  | \\ `'   /|   `-'  /           \n" +
+                " |  |  \\    /  \\      /           \n" +
+                " ''-'   `'-'    `-..-'              ");
+
+    }
+
+}
